646b1ca94c01e2d42b8da6527690db8bbd587474
Joshua Scott <joshua.ds.s019@gmail.com>
2024-09-16 12:06:12 -0300

    did some basic jsx review

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,35 @@ const App = () => {
 };
 
 export default App;
+
+// THE FOLLOWING WAS JUST AN INTRO TO JSX, PLEASE DISREGARD!!!
+// import React from 'react';
+
+// const App = () => {
+//   const name = 'Joshua';
+//   const x = 10;
+//   const y = 20;
+//   const names = ['Chandler', 'Joey', 'Ross', 'Monica', 'Rachel', 'Phoebe'];
+//   const loggedIn = false;
+//   const styles = {
+//     color: 'red',
+//     fontSize: '24px',
+//   };
+
+//   return (
+//     <>
+//       <p style={styles}>Hello {name}</p>
+//       <div>
+//         The sum of {x} and {y} is {x + y}.
+//       </div>
+//       <ul>
+//         {names.map((name, index) => (
+//           <li key={index}>{name}</li>
+//         ))}
+//       </ul>
+//       {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+//     </>
+//   );
+// };
+
+// export default App;
